9a21b85abc7aa8baaa01585761e53d57bbece0b5
Juerg Hunziker <juerg.hunziker@gmail.com>
2015-10-18 20:55:37 +0200

    fix header image / add front-page template

---
+++ web/content/themes/naturheilpraxis-ritter/front-page.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+<section class="main-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Naturheilpraxis Ritter</h1>
+                <p class="lead slogan">Gesund, Vital, Zufrieden</p>
+                <form>
+                    <div class="form-group text-center">
+                        <label for="exampleInputEmail1" class="sr-only">Problem?</label>
+                        <input type="text" class="form-control input-lg text-center" id="exampleInputEmail1" placeholder="Wo drückt der Schuh?">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container buffer-top">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Konsultieren Sie mich bei:</h2>
+            <ul>
+                <li>Kopfschmerzen</li>
+                <li>Erschöpfung</li>
+                <li>Verdauungsbeschwerden</li>
+                <li>...</li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h2>Für Ihr Wohlbefinden therapiere ich mittels:</h2>
+            <ul>
+                <li>Bioresonanz Austestung</li>
+                <li>Fussreflexzonen- und Gesundheits-Massage</li>
+                <li>Pflanzenheilkunde</li>
+                <li>Ausleitende Verfahren</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-xs-12">
+            <button class="btn btn-primary btn-lg" type="submit">Jetzt einen Termin finden</button>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
