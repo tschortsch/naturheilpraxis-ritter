be1f09e9ecb67489496e40e1ae8d8de4438b921d
Juerg Hunziker <juerg.hunziker@gmail.com>
2015-10-19 21:56:17 +0200

    fix redirect

--- web/content/themes/naturheilpraxis-ritter/assets/js/app.js
+++ web/content/themes/naturheilpraxis-ritter/assets/js/app.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     $("#ailment").select2({
         theme: "bootstrap",
         language: "de",
-        placeholder: l10n.ailment_placeholder
+        placeholder: l10n.ailment_placeholder,
+        allowClear: true
+    });
+
+    $("#ailment").change(function() {
+        var selected_ailment = $( this ).val();
+        window.location.href = "/suffering/" + selected_ailment;
     });
 });
